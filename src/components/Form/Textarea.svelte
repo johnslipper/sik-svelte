49b44d7a05fd5584b333a8bca6d2ff28bb3d66de
John Slipper <jslipper@me.com>
2020-12-23 16:51:50 +1300

    Added textarea component

---
+++ src/components/Form/Textarea.svelte
@@ -0,0 +1,68 @@
+<script>
+  export let id;
+  export let name;
+  export let placeholder;
+  export let maxlength = 255;
+  export let disabled = false;
+  export let value;
+</script>
+
+<style>
+  textarea {
+    outline: none;
+    font-size: 1rem;
+    padding: var(--inputPadding);
+    min-height: 6rem;
+    border: 1px solid var(--inputBorderColour);
+    border-radius: var(--inputBorderRadius);
+  }
+  textarea:focus {
+    box-shadow: var(--focusOutlineShadow);
+  }
+
+  /* Auto growing textarea (https://css-tricks.com/the-cleanest-trick-for-autogrowing-textareas/) */
+  .grow-wrap {
+    /* easy way to plop the elements on top of each other and have them both sized based on the tallest one's height */
+    display: grid;
+  }
+  .grow-wrap::after {
+    /* Note the weird space! Needed to preventy jumpy behavior */
+    content: attr(data-replicated-value) " ";
+
+    /* This is how textarea text behaves */
+    white-space: pre-wrap;
+
+    /* Hidden from view, clicks, and screen readers */
+    visibility: hidden;
+  }
+  .grow-wrap > textarea {
+    /* You could leave this, but after a user resizes, then it ruins the auto sizing */
+    resize: none;
+
+    /* Firefox shows scrollbar on growth, you can hide like this. */
+    overflow: hidden;
+  }
+  .grow-wrap > textarea,
+  .grow-wrap::after {
+    /* Identical styling required!! */
+    font-size: 1rem;
+    padding: var(--inputPadding);
+    min-height: 6rem;
+    border: 1px solid var(--inputBorderColour);
+    font: inherit;
+
+    /* Place on top of each other */
+    grid-area: 1 / 1 / 2 / 2;
+  }
+</style>
+
+<div class="grow-wrap">
+  <textarea
+    {id}
+    {name}
+    {placeholder}
+    {maxlength}
+    {disabled}
+    bind:value
+    onInput="this.parentNode.dataset.replicatedValue = this.value" />
+</div>
--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -2,3 +2,4 @@ export { default as Input } from "./Input.svelte";
 export { default as Label } from "./Label.svelte";
 export { default as Fieldset } from "./Fieldset.svelte";
 export { default as Legend } from "./Legend.svelte";
+export { default as Textarea } from "./Textarea.svelte";
