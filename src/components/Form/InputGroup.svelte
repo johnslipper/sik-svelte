860d416ff2d13ced672fb86cef3336c9d861c957
John Slipper <jslipper@me.com>
2020-12-24 16:09:30 +1300

    Added input group component

---
+++ src/components/Form/InputGroup.svelte
@@ -0,0 +1,10 @@
+<style>
+  .group {
+    display: grid;
+    gap: 0.25rem;
+  }
+</style>
+
+<div class="group">
+  <slot />
+</div>
--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -3,3 +3,4 @@ export { default as Label } from "./Label.svelte";
 export { default as Fieldset } from "./Fieldset.svelte";
 export { default as Legend } from "./Legend.svelte";
 export { default as Textarea } from "./Textarea.svelte";
+export { default as InputGroup } from "./InputGroup.svelte";
