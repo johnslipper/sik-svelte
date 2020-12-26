0652ec472e15f084f3e2849c218d0d10263e7995
John Slipper <jslipper@me.com>
2020-12-26 18:27:42 +1300

    Added basic Button component

---
+++ src/components/ui/Button/Button.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let type = "button";
+</script>
+
+<style>
+  button {
+    color: inherit;
+    background: none;
+    border: none;
+    padding: 0;
+  }
+  button:focus {
+    outline: var(--focusOutline);
+  }
+</style>
+
+<button {type} on:click>
+  <slot>Button</slot>
+</button>
--- src/components/ui/Button/index.js
+++ src/components/ui/Button/index.js
@@ -1,3 +1,4 @@
+export { default as Button } from "./Button.svelte";
 export { default as ButtonText } from "./ButtonText.svelte";
 export { default as ButtonDefault } from "./ButtonDefault.svelte";
 export { default as ButtonPrimary } from "./ButtonPrimary.svelte";
