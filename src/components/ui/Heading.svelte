3aa86f4ec0b861c5f21997a250032af0e1663fce
John Slipper <jslipper@me.com>
2020-12-27 12:46:18 +1300

    Heading component and variables. Removed global heading styles

---
+++ src/components/ui/Heading.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { theme } from "../../theme.js";
+  export let text = "Heading";
+  export let level = 1;
+  export let fontSize = "1.5rem";
+  let displayText = `<h${level} style="
+        font-size:${fontSize}; 
+        color: ${theme.headingColor};
+        font-weight: ${theme.headingWeight};
+        margin: 0;
+        ">${text}</h${level}>`;
+</script>
+
+{@html displayText}
