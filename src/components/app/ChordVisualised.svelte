5b10143711f2a5f98d0dda509ec9b3712ccbd57b
John Slipper <jslipper@me.com>
2020-12-31 08:30:24 +1300

    Fixed various issues resulting in incorrect chord rendering

--- src/components/app/ChordVisualised.svelte
+++ src/components/app/ChordVisualised.svelte
@@ -10,10 +10,15 @@
 
   onMount(() => {
     const tuningSplit = tuning ? tuning.split("") : null;
+    const fingeringArray = fingering.split(" ");
+    const fretsArray = processFrets(frets.split(" "));
+    const calculatedPosition = position || getPositionFromFrets(fretsArray);
+    const adjustedFrets = adjustFrets(fretsArray, calculatedPosition);
+
     new ChordBox(`#${key}`, { defaultColor: theme.bodyColor }).draw({
-      chord: getChord(fingering.split(" "), frets.split(" ")),
+      chord: getChord(fingeringArray, adjustedFrets),
       tuning: tuningSplit,
-      position: position || getPositionFromFrets(frets),
+      position: calculatedPosition,
     });
 
     // Add viewbox and fluid heights to allow container to scale responsively
@@ -23,24 +28,41 @@
     svgElement.setAttribute("height", "100%");
   });
 
+  // Process frets to be integers and lowercase "X"
+  function processFrets(frets) {
+    return frets.map((fret) => {
+      if (fret === "X") {
+        return fret.toLowerCase();
+      }
+      return parseInt(fret);
+    });
+  }
+
+  // Adjust valid fret numbers to start from neck position
+  function adjustFrets(frets, position) {
+    return frets.map((fret) => {
+      return fret && fret !== "x" ? fret - position + 1 : fret;
+    });
+  }
+
+  // Get chord as vexchords nested array
   function getChord(fingering, frets) {
     if (fingering.length !== frets.length) {
       return;
     }
-    return fingering.map((finger, i) => {
-      const fingerFormatted = finger === "X" ? null : finger;
-      const fretFormatted =
-        frets[i] === "X" ? frets[i].toLowerCase() : parseInt(frets[i]);
-      return [fingering.length - i, fretFormatted, fingerFormatted];
+    return frets.map((fret, i) => {
+      const fingerFormatted =
+        fingering[i] === "X" || fret === 0 ? null : fingering[i];
+      return [frets.length - i, fret, fingerFormatted];
     });
   }
 
+  // Find neck position based on lowest fret (can be overidden by position prop)
   function getPositionFromFrets(frets) {
     return Math.min(
       ...frets
-        .split(" ")
         .map((string) => {
-          if (string !== "X") {
+          if (string !== "x" && string !== 0) {
             return parseInt(string);
           }
         })
