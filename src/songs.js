1aae3ca74955588ea62133fe7411555e61442f30
John Slipper <jslipper@me.com>
2020-12-31 14:29:03 +1300

    Removed test chord name

--- src/songs.js
+++ src/songs.js
@@ -919,7 +919,6 @@ const songs = [
         title: "Verse",
         chords: [
           {
-            name: "Bob",
             voicing: "%2/1.X/X.0/0.2/2.3/3.0/0",
             fingering: "1 X 0 2 3 0",
             frets: "2 X 0 2 3 0",
