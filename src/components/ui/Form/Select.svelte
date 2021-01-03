9f8fd6755a185efd06156d9c1b0f1e41f054b543
John Slipper <jslipper@me.com>
2021-01-03 20:49:14 +1300

    Added Select and Option components

---
+++ src/components/ui/Form/Select.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let id;
+  export let name = "";
+  export let disabled = false;
+  export let required = false;
+  export let value;
+</script>
+
+<style>
+  select {
+    appearance: none;
+    outline: none;
+    width: 100%;
+    font-size: 1rem;
+    padding: 0.2rem 0.1rem;
+    border: 1px solid var(--inputBorderColor);
+    border-radius: var(--inputBorderRadius);
+    color: var(--bodyColor);
+  }
+  select:focus {
+    box-shadow: var(--focusOutlineShadow);
+  }
+</style>
+
+<select {id} {name} {disabled} {required} bind:value>
+  <slot />
+</select>
--- src/components/ui/Form/index.js
+++ src/components/ui/Form/index.js
@@ -5,3 +5,5 @@ export { default as Legend } from "./Legend.svelte";
 export { default as Textarea } from "./Textarea.svelte";
 export { default as FormGroup } from "./FormGroup.svelte";
 export { default as Form } from "./Form.svelte";
+export { default as Select } from "./Select.svelte";
+export { default as Option } from "./Option.svelte";
