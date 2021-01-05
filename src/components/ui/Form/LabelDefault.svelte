c60daa1c9d395dfa28b054a1f2f43d82225f5545
John Slipper <jslipper@me.com>
2021-01-06 12:37:52 +1300

    Added LabelDefault component

---
+++ src/components/ui/Form/LabelDefault.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let htmlFor;
+</script>
+
+<style>
+  label {
+    text-transform: uppercase;
+    font-size: 0.75em;
+    color: var(--neutralDark);
+    background-color: var(--neutralLightest);
+    padding: 0.25rem 0.5rem;
+    line-height: 1.5;
+  }
+</style>
+
+<label for={htmlFor}>
+  <slot />
+</label>
--- src/components/ui/Form/index.js
+++ src/components/ui/Form/index.js
@@ -1,5 +1,6 @@
 export { default as Input } from "./Input.svelte";
 export { default as Label } from "./Label.svelte";
+export { default as LabelDefault } from "./LabelDefault.svelte";
 export { default as Fieldset } from "./Fieldset.svelte";
 export { default as FieldsetDefault } from "./FieldsetDefault.svelte";
 export { default as Legend } from "./Legend.svelte";
