0df880fc88c802406ae2eb3b8487c4b8b2d9ddb3
John Slipper <jslipper@me.com>
2021-01-06 09:28:14 +1300

    Added fieldset default component that keeps a variation on browser default. Add Legend wrapper component.

--- src/components/ui/Form/index.js
+++ src/components/ui/Form/index.js
@@ -1,6 +1,8 @@
 export { default as Input } from "./Input.svelte";
 export { default as Label } from "./Label.svelte";
 export { default as Fieldset } from "./Fieldset.svelte";
+export { default as FieldsetDefault } from "./FieldsetDefault.svelte";
+export { default as Legend } from "./Legend.svelte";
 export { default as LegendSticky } from "./LegendSticky.svelte";
 export { default as Textarea } from "./Textarea.svelte";
 export { default as FormGroup } from "./FormGroup.svelte";
