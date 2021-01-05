1cf79cf4cbba7430319169ac9801e1a7290799ea
John Slipper <jslipper@me.com>
2021-01-06 09:29:38 +1300

    Implemented default fieldset component into chord edit fieldset in place of basic element

--- src/components/app/Chord/ChordEditFieldset.svelte
+++ src/components/app/Chord/ChordEditFieldset.svelte
@@ -1,17 +1,18 @@
 <script>
+  import { FieldsetDefault, Legend } from "../../ui/Form";
   export let string;
 </script>
 
 <style>
-  .fieldset {
+  .fields {
     display: grid;
     gap: 0.25rem;
-    margin: 0 0.15rem;
-    padding: 0.5rem;
   }
 </style>
 
-<fieldset class="fieldset">
-  <legend>{string}</legend>
-  <slot />
-</fieldset>
+<FieldsetDefault>
+  <Legend>{string}</Legend>
+  <div class="fields">
+    <slot />
+  </div>
+</FieldsetDefault>
