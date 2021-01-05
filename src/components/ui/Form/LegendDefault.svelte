63c4b709563b3bce02a0c59868eb601990571151
John Slipper <jslipper@me.com>
2021-01-06 11:58:05 +1300

    Added legend default component

---
+++ src/components/ui/Form/LegendDefault.svelte
@@ -0,0 +1,13 @@
+<style>
+  legend {
+    text-transform: uppercase;
+    font-size: 0.75em;
+    color: var(--neutralDark);
+    background-color: var(--neutralLightest);
+    padding: 0.25rem 0.5rem;
+  }
+</style>
+
+<legend>
+  <slot />
+</legend>
