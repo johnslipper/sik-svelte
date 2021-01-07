c8464d7895d2a145cbbb6ac6bb8c9bba7dbd4418
John Slipper <jslipper@me.com>
2021-01-07 17:18:57 +1300

    Added button link component to wrap router Link

---
+++ src/components/ui/Button/ButtonLink.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { Link } from "svelte-routing";
+  export let to;
+</script>
+
+<style>
+  .link :global(a) {
+    display: flex;
+    align-items: center;
+    padding: var(--buttonPadding);
+    gap: 0.25rem;
+    font-size: var(--buttonFontSize);
+    transition: transform 150ms;
+    text-transform: uppercase;
+  }
+
+  .link :global(a:focus) {
+    outline: var(--focusOutline);
+  }
+  .link :global(a:active) {
+    transform: scale(0.95);
+  }
+</style>
+
+<div class="link">
+  <Link {to}>
+    <slot />
+  </Link>
+</div>
