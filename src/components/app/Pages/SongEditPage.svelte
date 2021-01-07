1b110b036cdc180a46a496050447b1567f3f1c3a
John Slipper <jslipper@me.com>
2021-01-07 17:43:35 +1300

    Added save song button and skeleton save function

--- src/components/app/Pages/SongEditPage.svelte
+++ src/components/app/Pages/SongEditPage.svelte
@@ -6,8 +6,12 @@
   import VisuallyHidden from "../../ui/VisuallyHidden.svelte";
   export let id;
   import songs from "../../../songs.js";
-  import { ButtonLink } from "../../ui/Button";
+  import { ButtonText, ButtonLink } from "../../ui/Button";
   const song = songs[id];
+
+  function handleSave() {
+    // TODO
+  }
 </script>
 
 <Modal>
@@ -17,6 +21,9 @@
         <span>Cancel</span>
         <VisuallyHidden>editing song</VisuallyHidden>
       </ButtonLink>
+    </div>
+    <div slot="end" in:fade>
+      <ButtonText on:click={handleSave}>Save</ButtonText>
     </div>
   </AppHeader>
   <div class="page" in:fade>
