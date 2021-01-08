54da177bcc2229c05e3f76d9f2aab2cd29be8637
John Slipper <jslipper@me.com>
2021-01-08 13:57:19 +1300

    Song view header layout refinements

--- src/components/app/SongView/SongViewHeader.svelte
+++ src/components/app/SongView/SongViewHeader.svelte
@@ -16,48 +16,48 @@
 
 <style>
   header {
-    display: flex;
-    align-items: center;
     position: sticky;
     top: 0;
     z-index: 2;
-    padding: 0.5rem 0.5rem 1.5rem;
+    padding: 1rem 0.5rem;
     background-color: var(--primary);
     color: var(--primaryContrast);
+  }
+  .content {
+    display: flex;
+    align-items: center;
   }
   .wrapper {
     display: flex;
     gap: 0.75rem;
-    margin-bottom: -2.5rem;
     width: 100%;
+    margin-bottom: -2.5rem;
+    margin-top: -1rem;
   }
   .text {
+    padding-top: 0.25rem;
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    padding-top: 0.5rem;
-    padding-bottom: 2rem;
+  }
+  .title {
+    font-size: 0.85em;
   }
   .artist {
     font-size: 0.75em;
     text-transform: uppercase;
   }
   .album {
-    margin-top: 0.25em;
     font-size: 0.75em;
     font-style: italic;
   }
   .artwork {
     z-index: 1;
-    width: 5.5rem;
-    height: 5.5rem;
+    width: 5rem;
+    height: 5rem;
+    overflow: hidden;
+    flex-shrink: 0;
     border-radius: 50%;
     border: 0.25rem solid var(--primaryContrast);
-    overflow: hidden;
-  }
-  .start,
-  .end {
-    margin-top: 1rem;
   }
 
   .start :global(.link a) {
@@ -68,35 +68,36 @@
     display: flex;
     justify-content: flex-end;
     flex-grow: 1;
-    margin-right: 0.25em;
   }
 </style>
 
 <header>
-  <div class="start" in:fade={{ delay: animationDelay + 50 }}>
-    <ButtonLink to="/">
-      <Icon path={leftArrowIcon} size="1.1rem" />
-      <VisuallyHidden>back to songs</VisuallyHidden>
-    </ButtonLink>
-  </div>
-  <div class="wrapper">
-    <div
-      class="artwork"
-      in:fly={{ delay: animationDelay, duration: 300, x: -100, easing: backOut }}>
-      <ArtworkView {album} {artwork} />
+  <div class="content">
+    <div class="start" in:fade={{ delay: animationDelay + 50 }}>
+      <ButtonLink to="/">
+        <Icon path={leftArrowIcon} size="1.1rem" />
+        <VisuallyHidden>back to songs</VisuallyHidden>
+      </ButtonLink>
     </div>
-    <div
-      class="text"
-      in:fly={{ delay: animationDelay + 250, duration: 200, x: 50, easing: backOut }}>
-      {#if title}
-        <div class="title">{title}</div>
-      {/if}
-      {#if artist}
-        <div class="artist">{artist}</div>
-      {/if}
-      {#if album}
-        <div class="album">{album}</div>
-      {/if}
+    <div class="wrapper">
+      <div
+        class="artwork"
+        in:fly={{ delay: animationDelay, duration: 300, x: -100, easing: backOut }}>
+        <ArtworkView {album} {artwork} />
+      </div>
+      <div
+        class="text"
+        in:fly={{ delay: animationDelay + 250, duration: 200, x: 50, easing: backOut }}>
+        {#if title}
+          <div class="title">{title}</div>
+        {/if}
+        {#if artist}
+          <div class="artist">{artist}</div>
+        {/if}
+        {#if album}
+          <div class="album">{album}</div>
+        {/if}
+      </div>
     </div>
     <div class="end" in:fade={{ delay: animationDelay + 50 }}>
       <ButtonLink to="/song/{id}/edit">
