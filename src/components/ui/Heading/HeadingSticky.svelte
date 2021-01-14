dabd88a65370e50dee36bfcd4a3e95244c1d71ef
John Slipper <jslipper@me.com>
2021-01-14 17:48:26 +1300

    Added sticky heading component

---
+++ src/components/ui/Heading/HeadingSticky.svelte
@@ -0,0 +1,17 @@
+<style>
+  .wrapper {
+    display: block;
+    width: 100%;
+    padding: 0.5rem 1rem;
+    position: sticky;
+    top: var(--headerTopOffset);
+    text-transform: uppercase;
+    font-size: 0.85rem;
+    background-color: var(--neutralLightest);
+    z-index: 1;
+  }
+</style>
+
+<div class="wrapper">
+  <slot />
+</div>
