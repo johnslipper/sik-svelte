cd00f1cd0a6b7840ef083202e5e1563760403890
John Slipper <jslipper@me.com>
2021-01-15 16:56:15 +1300

    Swiched Heading component style vars from theme JS to CSS.

--- src/components/ui/Heading/Heading.svelte
+++ src/components/ui/Heading/Heading.svelte
@@ -1,12 +1,11 @@
 <script>
-  import { theme } from "../../../theme.js";
   export let text = "Heading";
   export let level = 1;
   export let fontSize = "1.2rem";
   let displayText = `<h${level} style="
         font-size:${fontSize}; 
-        color: ${theme.headingColor};
-        font-weight: ${theme.headingWeight};
+        color: var(--headingColor);
+        font-weight: var(--headingWeight);
         margin: 0;
         ">${text}</h${level}>`;
 </script>
