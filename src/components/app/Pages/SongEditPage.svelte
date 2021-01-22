9651cc991ebd5c115245d5a0d9547167ef6d9169
John Slipper <jslipper@me.com>
2021-01-22 17:18:22 +1300

    Added firebase to song edit page

--- src/components/app/Pages/SongEditPage.svelte
+++ src/components/app/Pages/SongEditPage.svelte
@@ -2,36 +2,41 @@
   import { fade } from "svelte/transition";
   import { navigate } from "svelte-routing";
   import Modal from "svelte-simple-modal";
+  import { User, Doc } from "sveltefire";
   import AppHeader from "../../ui/AppHeader.svelte";
   import SongEdit from "../SongEdit/SongEdit.svelte";
   import VisuallyHidden from "../../ui/VisuallyHidden.svelte";
   export let id;
-  import { songs } from "../../../songs.js";
   import { ButtonText, ButtonLink } from "../../ui/Button";
-  $: song = $songs && $songs[id];
 
-  function handleSave(song) {
-    // TODO
-    console.log(song);
-    navigate(`/songs/${id}`);
+  function handleSave(song, songRef) {
+    songRef.update(song).then(
+      () => navigate(`/songs/${id}`),
+      (error) => console.error(error)
+    );
   }
 </script>
 
-{#if $songs}
-  <Modal>
-    <AppHeader title="Edit song">
-      <div slot="start" in:fade>
-        <ButtonLink to="/songs/{id}">
-          <span>Cancel</span>
-          <VisuallyHidden>editing song</VisuallyHidden>
-        </ButtonLink>
+<Modal>
+  <User persist={sessionStorage}>
+    <Doc path={`/songs/${id}`} let:data={song} let:ref={songRef}>
+      <AppHeader title="Edit song">
+        <div slot="start" in:fade>
+          <ButtonLink to="/songs/{id}">
+            <span>Cancel</span>
+            <VisuallyHidden>editing song</VisuallyHidden>
+          </ButtonLink>
+        </div>
+        <div slot="end" in:fade>
+          <ButtonText on:click={() => handleSave(song, songRef)}
+            >Save</ButtonText
+          >
+        </div>
+      </AppHeader>
+      <div class="page" in:fade>
+        <SongEdit {song} isEdit="true" />
       </div>
-      <div slot="end" in:fade>
-        <ButtonText on:click={() => handleSave(song)}>Save</ButtonText>
-      </div>
-    </AppHeader>
-    <div class="page" in:fade>
-      <SongEdit bind:song isEdit="true"/>
-    </div>
-  </Modal>
-{:else}Loading...{/if}
+      <div slot="loading">Loading...</div>
+    </Doc>
+  </User>
+</Modal>
