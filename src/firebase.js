2f4c4073b9ec7db413d201b3e6c40eee17762bb6
John Slipper <jslipper@me.com>
2021-01-22 16:32:50 +1300

    Firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,6 @@
+export const config = {
+  apiKey: "AIzaSyAI1_JN9QSnvyPc53GskQ0t0DHCiUSmL-E",
+  authDomain: "songs-i-know-2020.firebaseapp.com",
+  databaseURL: "https://songs-i-know-2020.firebaseio.com",
+  projectId: "songs-i-know-2020",
+};
