a9635ac5fc36f7c95e4fcbded21989579be9c417
John Slipper <jslipper@me.com>
2021-01-22 17:17:36 +1300

    Split out song list placeholders into own component

--- src/components/app/SongsList/SongsList.svelte
+++ src/components/app/SongsList/SongsList.svelte
@@ -2,10 +2,7 @@
   import { fly } from "svelte/transition";
   import { backOut } from "svelte/easing";
   import SongsListItem from "./SongsListItem.svelte";
-  import PlaceholderItem from "./PlaceholderItem.svelte";
   export let songs;
-
-  let placeholderCount = 10;
 </script>
 
 <style>
@@ -15,31 +12,17 @@
     margin: 0;
     overflow: hidden;
   }
-  .placeholders {
-    display: flex;
-    flex-direction: column;
-    max-height: calc(100vh - var(--headerTopOffset));
-    overflow: hidden;
-  }
 </style>
 
-{#if songs}
-  <ul>
-    {#each songs as song, i}
-      <li in:fly={{ x: -20, delay: i * 75, easing: backOut, duration: 750 }}>
-        <SongsListItem
-          id={song.id}
-          title={song.title}
-          artist={song.artist}
-          album={song.album}
-          artwork={song.artwork} />
-      </li>
-    {/each}
-  </ul>
-{:else}
-  <div class="placeholders">
-    {#each Array(placeholderCount) as _placeholder}
-      <PlaceholderItem />
-    {/each}
-  </div>
-{/if}
+<ul>
+  {#each songs as song, i}
+    <li in:fly={{ x: -20, delay: i * 75, easing: backOut, duration: 750 }}>
+      <SongsListItem
+        id={song.id}
+        title={song.title}
+        artist={song.artist}
+        album={song.album}
+        artwork={song.artwork} />
+    </li>
+  {/each}
+</ul>
---
+++ src/components/app/SongsList/SongsListPlaceholders.svelte
@@ -0,0 +1,19 @@
+<script>
+  import PlaceholderItem from "./PlaceholderItem.svelte";
+  export let placeholderCount = 10;
+</script>
+
+<style>
+  .placeholders {
+    display: flex;
+    flex-direction: column;
+    max-height: calc(100vh - var(--headerTopOffset));
+    overflow: hidden;
+  }
+</style>
+
+<div class="placeholders">
+  {#each Array(placeholderCount) as _placeholder}
+    <PlaceholderItem />
+  {/each}
+</div>
