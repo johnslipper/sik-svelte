c8a564cd4da4bdd9c714d2a461aa8788aa6e7151
John Slipper <jslipper@me.com>
2021-01-22 17:17:51 +1300

    Added firebase to songs page

--- src/components/app/Pages/SongsPage.svelte
+++ src/components/app/Pages/SongsPage.svelte
@@ -3,12 +3,13 @@
   import { Link } from "svelte-routing";
   import AppHeader from "../../ui/AppHeader.svelte";
   import SongsList from "../SongsList/SongsList.svelte";
+  import SongsListPlaceholders from "../SongsList/SongsListPlaceholders.svelte";
   import VisuallyHidden from "../../ui/VisuallyHidden.svelte";
   import LogoCompact from "../Logo/LogoCompact.svelte";
   import Icon from "../../ui/Icons/Icon.svelte";
   import { plusIcon, cogIcon } from "../../ui/Icons/icons.js";
   import { ButtonLink } from "../../ui/Button";
-  import { songs } from "../../../songs.js";
+  import { User, Collection } from "sveltefire";
 </script>
 
 <style>
@@ -19,21 +20,40 @@
 
 <AppHeader>
   <div slot="start" in:fade>
-    <ButtonLink to="/settings">
-      <Icon path={cogIcon} size="1.1rem" />
-      <VisuallyHidden>Settings</VisuallyHidden>
-    </ButtonLink>
+    <User persist={sessionStorage}>
+      <ButtonLink to="/settings">
+        <Icon path={cogIcon} size="1.1rem" />
+        <VisuallyHidden>Settings</VisuallyHidden>
+      </ButtonLink>
+    </User>
   </div>
   <Link to="/">
     <LogoCompact />
   </Link>
   <div slot="end" in:fade>
-    <ButtonLink to="/add">
-      <Icon path={plusIcon} size="1.1rem" />
-      <VisuallyHidden>Add song</VisuallyHidden>
-    </ButtonLink>
+    <User persist={sessionStorage}>
+      <ButtonLink to="/add">
+        <Icon path={plusIcon} size="1.1rem" />
+        <VisuallyHidden>Add song</VisuallyHidden>
+      </ButtonLink>
+    </User>
   </div>
 </AppHeader>
 <div class="songsList">
-  <SongsList songs={$songs} />
+  <User let:user persist={sessionStorage}>
+    <Collection
+      path={"/songs"}
+      let:data={songs}
+      query={(ref) => ref.where("uid", "==", user.uid).orderBy("artist")}
+    >
+      <SongsList {songs} />
+      <div slot="loading">
+        <SongsListPlaceholders />
+      </div>
+    </Collection>
+
+    <div slot="signed-out">
+      Please <Link to="/login">login</Link> to start using the app
+    </div>
+  </User>
 </div>
