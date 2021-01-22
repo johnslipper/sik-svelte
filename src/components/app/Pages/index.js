26b734ce3888f2ee23271b3c9fef95651e5e79b9
John Slipper <jslipper@me.com>
2021-01-22 16:17:26 +1300

    Added page components index file

---
+++ src/components/app/Pages/index.js
@@ -0,0 +1,6 @@
+export { default as HomePage } from "./HomePage.svelte";
+export { default as IntroPage } from "./IntroPage.svelte";
+export { default as SettingsPage } from "./SettingsPage.svelte";
+export { default as SongAddPage } from "./SongAddPage.svelte";
+export { default as SongEditPage } from "./SongEditPage.svelte";
+export { default as SongViewPage } from "./SongViewPage.svelte";
