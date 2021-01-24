528ca45ce092305c90ed8ff7981e9619340766ed
John Slipper <jslipper@me.com>
2021-01-24 15:49:51 +1300

    Added button link default component

---
+++ src/components/ui/Button/ButtonLinkDefault.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { Link } from "svelte-routing";
+  export let to;
+</script>
+
+<div class="link">
+  <Link {to}>
+    <slot />
+  </Link>
+</div>
+
+<style>
+  .link :global(a) {
+    display: inline-block;
+    color: var(--buttonColor);
+    background: var(--buttonBackgroundColor);
+    border: 1px solid var(--buttonBorderColor);
+    padding: var(--buttonPadding);
+    border-radius: var(--buttonBorderRadius);
+    outline: none;
+    transition: background-color 150ms, transform 150ms;
+    font-size: var(--buttonFontSize);
+    text-transform: uppercase;
+  }
+
+  .link :global(a:focus) {
+    box-shadow: var(--focusOutlineShadow);
+  }
+  .link :global(a:active) {
+    color: var(--buttonActiveColor);
+    border-color: var(--buttonActiveBorderColor);
+    transform: scale(0.95);
+  }
+</style>
--- src/components/ui/Button/index.js
+++ src/components/ui/Button/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from "./Button.svelte";
 export { default as ButtonText } from "./ButtonText.svelte";
 export { default as ButtonLink } from "./ButtonLink.svelte";
+export { default as ButtonLinkDefault } from "./ButtonLinkDefault.svelte";
 export { default as ButtonDefault } from "./ButtonDefault.svelte";
 export { default as ButtonPrimary } from "./ButtonPrimary.svelte";
