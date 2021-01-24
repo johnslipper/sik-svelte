c6611eb4aa7a8c345506e330678cd3bb5cd4cf42
John Slipper <jslipper@me.com>
2021-01-24 15:36:58 +1300

    Default form autcomplete off

--- src/components/ui/Form/Form.svelte
+++ src/components/ui/Form/Form.svelte
@@ -1,11 +1,12 @@
 <script>
   export let onSubmit;
+  export let autocomplete = "off";
   function handleSubmit(e) {
     onSubmit(e);
     return e.preventDefault();
   }
 </script>
 
-<form on:submit={handleSubmit}>
+<form on:submit={handleSubmit} {autocomplete}>
   <slot />
 </form>
