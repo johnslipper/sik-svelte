62ecd8a14092d9ab2cc432f116b37ce1ab4fbb49
John Slipper <jslipper@me.com>
2021-01-26 13:41:20 +1300

    Songs list empty state

--- src/components/app/SongsList/SongsList.svelte
+++ src/components/app/SongsList/SongsList.svelte
@@ -5,19 +5,27 @@
   export let songs;
 </script>
 
-<ul>
-  {#each songs as song, i}
-    <li in:fly={{ x: -20, delay: i * 75, easing: backOut, duration: 750 }}>
-      <SongsListItem
-        id={song.id}
-        title={song.title}
-        artist={song.artist}
-        album={song.album}
-        artwork={song.artwork}
-      />
-    </li>
-  {/each}
-</ul>
+{#if songs}
+  {#if songs.length}
+    <ul>
+      {#each songs as song, i}
+        <li in:fly={{ x: -20, delay: i * 75, easing: backOut, duration: 750 }}>
+          <SongsListItem
+            id={song.id}
+            title={song.title}
+            artist={song.artist}
+            album={song.album}
+            artwork={song.artwork}
+          />
+        </li>
+      {/each}
+    </ul>
+  {:else}
+    <p class="empty">
+      No songs currently saved. Use the plus button to get started
+    </p>
+  {/if}
+{/if}
 
 <style>
   ul {
@@ -25,5 +33,9 @@
     padding: 0;
     margin: 0;
     overflow: hidden;
+  }
+  .empty {
+    padding: 0 var(--contentPaddingHorizontal);
+    color: var(--bodyColorMuted);
   }
 </style>
