aac2563f56350d497934bff8fa712411523fd301
John Slipper <jslipper@me.com>
2021-01-27 16:27:46 +1300

    Added welcome page

--- src/components/app/Pages/SongsPage.svelte
+++ src/components/app/Pages/SongsPage.svelte
@@ -4,6 +4,7 @@
   import AppHeader from "../../ui/AppHeader.svelte";
   import SongsList from "../SongsList/SongsList.svelte";
   import SongsListPlaceholders from "../SongsList/SongsListPlaceholders.svelte";
+  import Welcome from "../Welcome.svelte";
   import VisuallyHidden from "../../ui/VisuallyHidden.svelte";
   import LogoCompact from "../Logo/LogoCompact.svelte";
   import Icon from "../../ui/Icons/Icon.svelte";
@@ -12,49 +13,55 @@
   import { User, Collection } from "sveltefire";
 </script>
 
-<AppHeader>
-  <div slot="start" in:fade>
-    <User persist={sessionStorage}>
-      <ButtonLink to="/settings">
-        <Icon path={cogIcon} size="1.1rem" />
-        <VisuallyHidden>Settings</VisuallyHidden>
-      </ButtonLink>
-    </User>
-  </div>
-  <Link to="/">
-    <LogoCompact />
-  </Link>
-  <div slot="end" in:fade>
-    <User persist={sessionStorage}>
-      <ButtonLink to="/add">
-        <Icon path={plusIcon} size="1.1rem" />
-        <VisuallyHidden>Add song</VisuallyHidden>
-      </ButtonLink>
-    </User>
-  </div>
-</AppHeader>
-<div class="list">
-  <User let:user persist={sessionStorage}>
-    <Collection
-      path={`/users/${user.uid}/songs`}
-      let:data={songs}
-      query={(ref) => ref.orderBy("artist")}
-    >
-      <SongsList {songs} />
-      <div slot="loading">
-        <SongsListPlaceholders />
-      </div>
-    </Collection>
-
-    <div slot="signed-out">
-      Please <Link to="/sign-in">sign in</Link> to start using the app
+<div class="wrapper">
+  <AppHeader>
+    <div slot="start" in:fade>
+      <User persist={sessionStorage}>
+        <ButtonLink to="/settings">
+          <Icon path={cogIcon} size="1.1rem" />
+          <VisuallyHidden>Settings</VisuallyHidden>
+        </ButtonLink>
+      </User>
     </div>
-  </User>
+    <Link to="/">
+      <LogoCompact />
+    </Link>
+    <div slot="end" in:fade>
+      <User persist={sessionStorage}>
+        <ButtonLink to="/add">
+          <Icon path={plusIcon} size="1.1rem" />
+          <VisuallyHidden>Add song</VisuallyHidden>
+        </ButtonLink>
+      </User>
+    </div>
+  </AppHeader>
+  <div class="list">
+    <User let:user persist={sessionStorage}>
+      <Collection
+        path={`/users/${user.uid}/songs`}
+        let:data={songs}
+        query={(ref) => ref.orderBy("artist")}
+      >
+        <SongsList {songs} />
+        <div slot="loading">
+          <SongsListPlaceholders />
+        </div>
+      </Collection>
+
+      <div class="welcome" slot="signed-out">
+        <Welcome />
+      </div>
+    </User>
+  </div>
 </div>
 
 <style>
-  .list {
+  .wrapper {
     --headerTopOffset: 3.5rem;
+  }
+  .wrapper,
+  .list,
+  .welcome {
     display: flex;
     flex-direction: column;
     flex-grow: 1;
---
+++ src/components/app/Welcome.svelte
@@ -0,0 +1,45 @@
+<script>
+  import { ButtonLinkDefault } from "../ui/Button";
+  import Heading from "../ui/Heading/Heading.svelte";
+  import LogoMark from "./Logo/LogoMark.svelte";
+</script>
+
+<div class="wrapper">
+  <div class="logo">
+    <LogoMark />
+  </div>
+  <Heading text="Welcome to Songs I Know" level="2" fontSize="1.5rem" />
+  <p>Collect songs you've learned to find inspiration when you need it</p>
+
+  <div class="actions">
+    <ButtonLinkDefault to="/sign-in">Sign in</ButtonLinkDefault>
+    <ButtonLinkDefault to="/sign-up">Sign up</ButtonLinkDefault>
+  </div>
+</div>
+
+<style>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    justify-content: center;
+    padding: 2rem;
+    margin: 0 auto;
+    text-align: center;
+    max-width: 25rem;
+  }
+  .logo {
+    max-width: 10rem;
+    opacity: 0.85;
+    margin: 0 auto 1rem;
+  }
+  p {
+    margin: 1rem 0 1.5rem;
+    color: var(--bodyColorMuted);
+  }
+  .actions {
+    display: flex;
+    gap: 0.75rem;
+    justify-content: center;
+  }
+</style>
