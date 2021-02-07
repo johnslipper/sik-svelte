d5a1352255b01744256745effa7270c92e24c9c5
John Slipper <jslipper@me.com>
2021-02-07 14:38:36 +1300

    Added danger button component

---
+++ src/components/ui/Button/ButtonDanger.svelte
@@ -0,0 +1,25 @@
+<script>
+  import ButtonDefault from "./ButtonDefault.svelte";
+  export let type = "button";
+  export let disabled = false;
+</script>
+
+<div class="buttonDanger">
+  <ButtonDefault {type} {disabled} on:click>
+    <slot>Button</slot>
+  </ButtonDefault>
+</div>
+
+<style>
+  .buttonDanger {
+    display: inline-block;
+  }
+  .buttonDanger :global(button) {
+    --buttonBorderColor: transparent;
+    --buttonBackgroundColor: var(--danger);
+    --buttonColor: var(--primaryContrast);
+    --buttonActiveBorderColor: transparent;
+    --buttonActiveBackgroundColor: var(--dangerDark);
+    --buttonActiveColor: var(--primaryContrast);
+  }
+</style>
