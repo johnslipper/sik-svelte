3c5117aeea002d14e97c443403c4de15cbddfe20
John Slipper <jslipper@me.com>
2021-03-06 16:47:06 +1300

    Password reset page layout improvements

--- src/components/app/Pages/PasswordResetPage.svelte
+++ src/components/app/Pages/PasswordResetPage.svelte
@@ -2,6 +2,7 @@
   import { User } from "sveltefire";
   import { redirectIfUser } from "../../../firebase.js";
   import AppHeader from "../../ui/AppHeader.svelte";
+  import PaperAirplaneIcon from "../../ui/Icons/PaperAirplaneIcon.svelte";
   import { Button, ButtonLink } from "../../ui/Button";
   import {
     Form,
@@ -34,43 +35,50 @@
   persist={localStorage}
   on:user={(e) => redirectIfUser(e.detail.user)}
 >
-  <div slot="signed-out">
-    <div class="wrapper">
-      {#if isSent}
-        <p>A password reset email has been sent, check your inbox</p>
+  <div class="wrapper" slot="signed-out">
+    {#if isSent}
+      <div class="success">
+        <div class="icon">
+          <PaperAirplaneIcon />
+        </div>
+
+        <p>A password reset email has been sent to <strong>{email}</strong></p>
         <div class="actions">
           <ButtonLink variant="default" to="/sign-in"
             >To sign in page</ButtonLink
           >
         </div>
-      {:else}
-        <Form onSubmit={() => handleSubmit(auth)}>
-          <div class="form">
-            <FormGroup>
-              <LabelDefault htmlFor="resetEmail">Email</LabelDefault>
-              <Input
-                id="resetEmail"
-                required="true"
-                type="email"
-                bind:value={email}
-                on:blur={resetError}
-              />
-            </FormGroup>
-            <div class="buttons">
-              <Button variant="primary" type="submit">Reset password</Button>
-              <ButtonLink to="/sign-in">To sign in</ButtonLink>
-            </div>
-            <FormError message={error} />
+      </div>
+    {:else}
+      <Form onSubmit={() => handleSubmit(auth)}>
+        <div class="form">
+          <FormGroup>
+            <LabelDefault htmlFor="resetEmail">Email</LabelDefault>
+            <Input
+              id="resetEmail"
+              required="true"
+              type="email"
+              bind:value={email}
+              on:blur={resetError}
+            />
+          </FormGroup>
+          <div class="buttons">
+            <Button variant="primary" type="submit">Reset password</Button>
+            <ButtonLink to="/sign-in">To sign in</ButtonLink>
           </div>
-        </Form>
-      {/if}
-    </div>
+          <FormError message={error} />
+        </div>
+      </Form>
+    {/if}
   </div>
 </User>
 
 <style>
   .wrapper {
-    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    padding: var(--contentPaddingHorizontal);
+    flex-grow: 1;
   }
   .form {
     display: grid;
@@ -81,7 +89,22 @@
     gap: 0.25rem;
     justify-content: space-between;
   }
+  .success {
+    display: grid;
+    justify-content: center;
+    align-content: center;
+    gap: 1.5rem;
+    text-align: center;
+    flex-grow: 1;
+  }
   .actions {
-    display: inline-block;
+    display: flex;
+    justify-content: center;
+  }
+  .icon {
+    width: 10rem;
+    height: auto;
+    margin: 0 auto;
+    opacity: 0.75;
   }
 </style>
