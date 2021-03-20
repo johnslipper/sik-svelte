e5c279681e101bc5e5665863bb34d9c19f956099
John Slipper <jslipper@me.com>
2021-03-21 09:06:46 +1300

    Changed chord play message modals to toasts

--- src/components/app/Chord/ChordPlay.svelte
+++ src/components/app/Chord/ChordPlay.svelte
@@ -2,32 +2,23 @@
   import { getContext } from "svelte";
   import { playChord } from "./ChordAudio.js";
   import VisuallyHidden from "../../ui/VisuallyHidden.svelte";
-  import Dialogue from "../../ui/Modal/Dialogue.svelte";
   import { Button } from "../../ui/Button";
   import { speakerIcon } from "../../ui/Icons/icons.js";
   import Icon from "../../ui/Icons/Icon.svelte";
+  import { infoToast, errorToast } from "../../ui/Toasts/toasts.js";
   export let chord;
   export let tuning;
   export let capoAdjustment;
 
-  const { open } = getContext("simple-modal");
-
   function handlePlayChord() {
     if (chord.frets) {
       if (tuning && tuning !== "E A D G B E") {
-        open(Dialogue, {
-          CancelText: undefined,
-          message: "Cannot play chords with custom tuning yet sorry",
-          showCancelButton: false,
-        });
+        infoToast("Cannot play chords with custom tuning yet sorry");
       } else {
         playChord(chord.frets, capoAdjustment);
       }
     } else {
-      open(Dialogue, {
-        CancelText: undefined,
-        message: "Cannot play chord, no fret data",
-      });
+      errorToast("Cannot play chord, no fret data");
     }
   }
 </script>
