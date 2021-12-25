83a58054993861b22332157ef4b17e94d754984c
John Slipper <jslipper@me.com>
2021-12-26 11:47:14 +1300

    Switch rollup for vite and remove service worker
    Will look to reimplement service worker later

--- src/main.js
+++ src/main.js
@@ -2,14 +2,6 @@ import App from "./components/App.svelte";
 
 const app = new App({
   target: document.body,
-  hydratable: true,
 });
-
-if (process.env.isProd && "serviceWorker" in navigator) {
-  // Use the window load event to keep the page load performant
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js");
-  });
-}
 
 export default app;
