a6d86e1520b1ef13d0be9d86c84f0592fd5cb754
John Slipper <jslipper@me.com>
2022-01-16 12:16:06 +1300

    Add songs context CRUD functions
    Remove default sample song addition on store initialisation

--- src/app/Song/SongStorage.svelte
+++ src/app/Song/SongStorage.svelte
@@ -1,22 +1,48 @@
 <script lang="ts" context="module">
   import { persist, indexedDBStorage } from "@macfja/svelte-persistent-store";
   import { writable } from "svelte/store";
-  import sampleSong from "./SongSample";
+  import type { Song } from "./index";
 
-  const songs = persist(
-    writable([{ ...sampleSong, id: 1 }]),
-    indexedDBStorage(),
-    "songs"
-  );
+  const songs = persist(writable([]), indexedDBStorage(), "songs");
 
   export const songStorageContext = {};
 </script>
 
 <script lang="ts">
   import { setContext } from "svelte";
+
+  function getSong(id: number) {
+    return $songs.find((song) => song.id === id);
+  }
+
+  function addSong(song: Song) {
+    return songs.update((songs: Song[]) => {
+      const id = songs.length + 1;
+      return [...songs, { ...song, id }];
+    });
+  }
+
+  function updateSong(song: Song) {
+    return songs.update((songs: Song[]) => {
+      const otherSongs = songs.filter(
+        (currentSong) => currentSong.id !== song.id
+      );
+      return [...otherSongs, song];
+    });
+  }
+
+  function removeSong({ id }: Song) {
+    return songs.update((songs: Song[]) => {
+      return songs.filter((song) => song.id !== id);
+    });
+  }
+
   setContext(songStorageContext, {
     songs,
-    getSong: (id: number) => $songs.find((song) => song.id === id),
+    getSong,
+    addSong,
+    updateSong,
+    removeSong,
   });
 </script>
 
