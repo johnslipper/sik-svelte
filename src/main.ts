e29027a18be1ba8ad5f923a8cf847084d0d16d8c
John Slipper <jslipper@me.com>
2022-01-24 10:31:02 +1300

    Register service worker
    Prompt for reload when update available
    Offline ready alert

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import App from "./app/App.svelte";
+import { registerSW } from "virtual:pwa-register";
 
 const app = new App({
   target: document.body,
+});
+
+// Register service worker
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("A new version is available, update now?")) {
+      updateSW();
+    }
+  },
+  onOfflineReady() {
+    alert("Offline mode is now ready!");
+  },
 });
 
 export default app;
